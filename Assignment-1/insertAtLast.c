0248a7c0e5fd2fc5077be7730ca016d004a08f92
Dhivyashree56 <126873314+Dhivyashree56@users.noreply.github.com>
2023-03-09 17:41:24 +0530

    Create insertAtLast.c

---
+++ Assignment-1/insertAtLast.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct node
+{
+    int data;
+    struct node *next;
+};
+void insertAtLast(struct node **head,int n){
+    struct node *newNode=(struct node*)malloc(sizeof(struct node));
+    newNode->data=n;
+    newNode->next=NULL;
+    struct node *temp=*head;
+    if(*head==NULL){
+        *head=newNode;
+    }
+    else{
+        while(temp->next!=NULL){
+            temp=temp->next;
+        }
+        temp->next=newNode;
+    }
+}
+void display(struct node *head){
+    struct node *temp=head;
+    printf("The elements in the linked list are:\n");
+    while(temp!=NULL){
+        printf("%d->",temp->data);
+        temp=temp->next;
+    }
+    printf("NULL");
+}
+int main(){
+    struct node *head=NULL;
+    int n,d;
+    printf("Enter the no.of.nodes:");
+    scanf("%d",&n);
+    for(int i=0;i<n;i++){
+        scanf("%d",&d);
+        insertAtLast(&head,d);
+    }
+    display(head);
+    return 0;
+}
